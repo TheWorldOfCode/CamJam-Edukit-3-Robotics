ab3821051abf93979b3473ae62bd47d28aa116f2
AC12344 <annec.poulsen@gmail.com>
2019-11-20 11:29:03 +0100

    added a new file

---
+++ ultra_sound/getDist_test.py
@@ -0,0 +1,13 @@
+from gpiozero import DistanceSensor # imported so we can get the distances.
+import argparse # imported to get input from a bash script
+
+#Define GPIO pins for the distance sensor
+pinTrigger = 17
+pinEcho = 18
+
+#setting up the imported functions
+sensor = DistanceSensor(echo = pinEcho, trigger = pinTrigger)
+
+distance = sensor.distance *100
+
+print(str(distance))
